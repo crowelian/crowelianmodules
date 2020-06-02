16213940d3b9a24a87aeca683fa924e66b01988d
Harri Ahola <harriahola@live.fi>
2020-06-02 19:03:22 +0300

    - added create project folders version 1

---
+++ Assets/CrowelianModules/Scripts/CreateProjectFolders.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateProjectFolders : MonoBehaviour
+{
+
+    [MenuItem("Tools/Create Project Folders")]
+    static void CreateAllProjectFolders()
+    {
+        string guid = AssetDatabase.CreateFolder("Assets", "Scripts");
+        string newFolderPath = AssetDatabase.GUIDToAssetPath(guid);
+
+        guid = AssetDatabase.CreateFolder("Assets", "Textures");
+        newFolderPath = AssetDatabase.GUIDToAssetPath(guid);
+
+        guid = AssetDatabase.CreateFolder("Assets", "Anims");
+        newFolderPath = AssetDatabase.GUIDToAssetPath(guid);
+
+        guid = AssetDatabase.CreateFolder("Assets", "Models");
+        newFolderPath = AssetDatabase.GUIDToAssetPath(guid);
+
+        guid = AssetDatabase.CreateFolder("Assets", "Audio");
+        newFolderPath = AssetDatabase.GUIDToAssetPath(guid);
+    }
+
+   
+}
